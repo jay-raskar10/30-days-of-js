4752525bc1074ac8c3be39fc8c2b277ff36958bd
Jay Raskar <jayraskar2003@gmail.com>
2024-07-15 23:18:14 +0530

    Day 2 completed: operators

---
+++ day_2/operators.js
@@ -0,0 +1,89 @@
+//Activity 1 : Arithmetic Operations
+let a = 8;
+let b = 5;
+
+//Task 1: Addition
+// console.log(`Addition of ${a} + ${b} is :${a + b}`);
+
+//Task 2: Subtraction
+// console.log(`Subtraction of ${a} - ${b} is :${a - b}`);
+
+//Task 3: Multiplication
+// console.log(`Multiplication of ${a} * ${b} is :${a * b}`);
+
+//Task 4: Division
+// console.log(`Division of ${a} / ${b} is :${a / b}`);
+
+//Task 5: Divide for Remainder
+// console.log(`Remainder of ${a} divided by ${b} is :${a % b}`);
+
+//---------------------------------------------------------------------------------------
+
+//Activity 2:  Assignment Operators
+let abc = 5;
+
+//Task 6:
+abc += 4;
+// console.log(abc);
+
+//Task 7:
+abc -= 6;
+// console.log(abc);
+
+//---------------------------------------------------------------------------------------
+
+//Activity 3:  Assignment Operators
+
+//Task 8:
+if (5 > 4) {
+    // console.log("Num1 is greater than Num2");
+}
+if (4 < 5) {
+    // console.log("Num1 is less than Num2");
+}
+
+//Task 9:
+if (5 >= 5) {
+    // console.log("Num1 is greater than or equal to Num2");
+}
+if (3 <= 4) {
+    // console.log("Num1 is less than or equal to Num2");
+}
+
+//Task 10:
+if ('22' == 22) {
+    // console.log("Used == equality check operator '22' == 22 : equality check");
+}
+
+if ('22' === '22') {
+    // console.log("Used === strictly equal operator '22' === '22' : not equal to");
+}
+
+//---------------------------------------------------------------------------------------
+
+//Activity 4:  Logical Operators
+
+//Task 11:
+if (5 > 4 && 4 < 5) {
+    // console.log(`&& operator used`);
+}
+
+//Task 12:
+if (5 > 4 || 4 < 5) {
+    // console.log(`|| operator used`);
+}
+
+//Task 13:
+let val = true;
+// console.log(!val)
+
+//---------------------------------------------------------------------------------------
+
+//Activity 5:  Ternary Operators
+
+let value = 0;
+value >= 0 ? console.log('Number is positive') : console.log('Number is negative');
+
+//************************************************************************************************************************//
+
+
